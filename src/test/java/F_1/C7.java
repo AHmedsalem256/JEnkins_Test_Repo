0b5fa49eeece2b2240998c3d35fe2418132fd394
Ahmed Salem <93452552+AHmedsalem256@users.noreply.github.com>
2025-03-30 21:51:58 +0200

    Update C7.java

--- src/test/java/F_1/C7.java
+++ src/test/java/F_1/C7.java
@@ -3,12 +3,7 @@ package F_1;
 public class C7 {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("KAsELO");
-		System.out.println("PIMPO");
-		System.out.println("PIMPO");
-		
-		
+		System.out.println("I LOVE YOU MAVEN");
 	}
 	
 }
